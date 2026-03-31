236046e7f365aa240dbe0c2e3d1612686004191b
yuhaibo <iahob@icloud.com>
2026-03-31 21:12:47 +0800

    Refactor docker-compose and update get-client-qr.sh for improved configuration handling

--- get-client-qr.sh
+++ get-client-qr.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
+
+CONFIG_DIR="/opt/xray/config"
 EXT_IP=$(curl -s ifconfig.me)
 UUID=$(cat config/uuid)
 PUB_KEY=$(cat config/public)
+LINK="vless://${UUID}@${EXT_IP}:443?security=reality&encryption=none&pbk=${PUB_KEY}&headerType=none&fp=chrome&type=tcp&flow=xtls-rprx-vision&sni=${SNI}&sid=${SHORT_ID}#MyVLESS"
 
-echo "vless://${UUID}@${EXT_IP}:443?security=reality&encryption=none&pbk=${PUB_KEY}&headerType=none&fp=chrome&type=tcp&flow=xtls-rprx-vision&sni=${SNI}&sid=${SHORT_ID}#MyVLESS" > config/client_qr.txt
-qrencode -t ansiutf8 < config/client_qr.txt
+
+# 输出二维码
+echo "$LINK" > ${CONFIG_DIR}/client_qr.txt
+qrencode -t ansiutf8 < ${CONFIG_DIR}/client_qr.txt
+
+base64 -w 0 ${CONFIG_DIR}/client_qr.txt > ${CONFIG_DIR}/sub
